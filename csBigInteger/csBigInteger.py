563f4ed1184ed253d4f7b3aad5e303c170f53ef0
Igor Machado <igor.machado@gmail.com>
2019-07-12 14:23:35 -0300

    trying toInt with multiple values

--- csBigInteger/csBigInteger.py
+++ csBigInteger/csBigInteger.py
@@ -14,25 +14,28 @@ csbiginteger_lib.csbiginteger_init_empty.restype  = ctypes.c_int
 #to_int (32 bits)
 csbiginteger_lib.csbiginteger_to_int.argtypes = [ctypes.c_void_p, ctypes.c_int]
 csbiginteger_lib.csbiginteger_to_int.restype  = ctypes.c_int
+# csbiginteger_init_i
+csbiginteger_lib.csbiginteger_init_i.argtypes = [ctypes.c_int, ctypes.c_void_p, ctypes.c_int]
+csbiginteger_lib.csbiginteger_init_i.restype  = ctypes.c_int
 
 
 class BigInteger:
-    def __init__(self):
+    # how to pass null parameters, or multiple?
+    def __init__(self, intv = 0):
         self.std_size = 256 # 256 bytes, standard size (TODO: improve this with better logic, but now it's enough)
         self._datasize = self.std_size
         self._data = (ctypes.c_uint8*self._datasize)()
-        sz = self.initEmpty(self._data, self._datasize)
+        if intv == 0:
+            sz = csbiginteger_lib.csbiginteger_init_empty(self._data, self._datasize)
+        else:
+            sz = csbiginteger_lib.csbiginteger_init_i(intv, self._data, self._datasize)
         if sz == 0:
             raise ValueError('Something wrong with BigInteger. Zero size.')
-        self._length = sz
-
-    # auxiliar method, make it private (and static)
-    def initEmpty(self, data, datasize):
-        sz = csbiginteger_lib.csbiginteger_init_empty(data, datasize)
-        return sz
+        self._length = sz    
 
     def length(self):
         return self._length
 
     def toInt(self):
         return csbiginteger_lib.csbiginteger_to_int(self._data, self._datasize)
+
--- test.py
+++ test.py
@@ -9,6 +9,15 @@ def main():
     big = BigInteger()
     print 'length = ', big.length()
     print 'toInt = ', big.toInt()
+
+    big1 = BigInteger(1)
+    print 'length = ', big1.length()
+    print 'toInt = ', big1.toInt()
+
+    bigM1 = BigInteger(-1)
+    print 'length = ', bigM1.length()
+    print 'toInt = ', bigM1.toInt()
+
     return 0
 
 if __name__ == '__main__':
