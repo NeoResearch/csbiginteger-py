8b8c43f97348917ae512a75eddfd065e3ab6a41a
Igor Machado <igor.machado@gmail.com>
2019-07-12 15:41:44 -0300

    long print

--- README.md
+++ README.md
@@ -18,11 +18,20 @@ def main():
     print 'length = ', big.length()
     print 'toInt = ', big.toInt()
     print 'toString: ', big.toString()
+    print ''
 
     bigM1 = BigInteger(-1)
     print 'length = ', bigM1.length()
     print 'toInt = ', bigM1.toInt()
     print 'toString: ', bigM1.toString()
+    print ''
+
+    big4293967296 = BigInteger(4293967296)
+    print 'length = ', big4293967296.length()
+    print 'toInt = ', big4293967296.toInt()
+    print 'toLong = ', big4293967296.toLong()
+    print 'toString: ', big4293967296.toString()
+
     return 0
 ```
 
@@ -32,9 +41,15 @@ Empty BigInteger is expected to be byte array 0x00, with size = 1 (byte) and int
 length =  1
 toInt =  0
 toString:  0x00
+
 length =  1
 toInt =  -1
 toString:  0xff
+
+length =  5
+toInt =  -1000000
+toLong =  4293967296
+toString:  0x00fff0bdc0
 ```
 
 ## LICENSE
--- test.py
+++ test.py
@@ -10,27 +10,32 @@ def main():
     print 'length = ', big.length()
     print 'toInt = ', big.toInt()
     print 'toString: ', big.toString()
+    print ''
 
     big1 = BigInteger(1)
     print 'length = ', big1.length()
     print 'toInt = ', big1.toInt()
     print 'toString: ', big1.toString()
+    print ''
 
     bigM1 = BigInteger(-1)
     print 'length = ', bigM1.length()
     print 'toInt = ', bigM1.toInt()
     print 'toString: ', bigM1.toString()
+    print ''
 
     big255 = BigInteger(255)
     print 'length = ', big255.length()
     print 'toInt = ', big255.toInt()
     print 'toString: ', big255.toString()
+    print ''
 
     bigM10M = BigInteger(-1000000)
     print 'length = ', bigM10M.length()
     print 'toInt = ', bigM10M.toInt()
     print 'toLong = ', bigM10M.toLong()
     print 'toString: ', bigM10M.toString()
+    print ''
 
     big4293967296 = BigInteger(4293967296)
     print 'length = ', big4293967296.length()
